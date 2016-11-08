43d5a69e2b9c68255de24157a1fcfd2a51c71360
justinewalt <justinewalt@gmail.com>
2016-11-07 22:23:00 -0700

    built views for posting a bid

---
+++ app/views/bids/config/routes.rb
@@ -0,0 +1,35 @@
+Rails.application.routes.draw do
+
+  root 'home#index'
+
+  # devise_for :hauliers, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+  # devise_for :shippers, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+  # devise_for :employees, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+
+
+  devise_for :hauliers, controllers: {
+        sessions: 'hauliers/sessions',
+        registrations: 'hauliers/registrations'
+      }
+
+  devise_for :shippers, controllers: {
+        sessions: 'shippers/sessions',
+        registrations: 'shippers/registrations'
+      }
+
+  devise_for :employees, controllers: {
+        sessions: 'shippers/sessions',
+        registrations: 'shippers/registrations'
+      }
+
+  # devise_for :shippers
+  # devise_for :hauliers
+
+  resources :hauliers do
+    resources :jobs
+  end
+
+  resources :shippers do
+    resources :jobs
+  end
+end
