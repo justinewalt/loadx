5e426c3b1b6f17f16f0e28c5a976f6f9426a67da
justinewalt <justinewalt@gmail.com>
2016-11-09 19:15:35 -0700

    built seeder with haulier and shippers

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,8 @@ Shipper.create( email: 'dave@dave.com', password: 'password',
                 sub_district: 'dave', district: 'dave',
                 province: 'dave', country: 'dave', postal_code: 'dave',
                 office_phone: 'dave', contact_person: 'dave', tax_id: 'dave')
+Haulier.create( email: 'mike@mike.com', password: 'password',
+                company_name: 'mike', street_address: 'mike',
+                sub_district: 'mike', district: 'mike',
+                province: 'mike', country: 'mike', postal_code: 'mike',
+                office_phone: 'mike', contact_person: 'mike', tax_id: 'mike')
